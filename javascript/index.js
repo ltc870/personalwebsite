90f70e585d69fe1cd30714f1939573f07657a245
Lawrence C <lawrencecaudle870@gmail.com>
2021-12-04 00:03:27 -0500

    updated html and javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,11 @@
 const closeButton = document.querySelector('.close-navigation');
 const openButton = document.querySelector('.open-navigation');
 const nav = document.querySelector('.navigation');
+const aboutEl = document.querySelector('.about')
+const techEl = document.querySelector('.tech')
+const projectEl = document.querySelector('.project')
+const contactEl = document.querySelector('.contact')
+const socialEl = document.querySelector('.social')
 
 
 closeButton.addEventListener("click", () => {
