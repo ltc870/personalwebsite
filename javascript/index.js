7a1dcbf3eab1a66b866fc04bc8c67c55f0bbfd41
Lawrence C <lawrencecaudle870@gmail.com>
2021-12-04 21:48:50 -0500

    added comments about links in javascript

--- javascript/index.js
+++ javascript/index.js
@@ -9,38 +9,47 @@ const socialEl = document.querySelector('.social');
 const navNameEl = document.querySelector('.navigation-name');
 
 
+
+// To close the navigation window
 closeButton.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
 });
 
+// To add the navigation window
 openButton.addEventListener("click", () => {
   nav.classList.add('navigation-open');
 });
 
+// When the "navigation-name" is clicked, the browser will scroll to the top.
 navNameEl.addEventListener("click", () => {
   scrollTo({top: 0, left: 0, behavior: "smooth"});
 });
 
+// When the "about" link is clicked, then the browser will scroll to the "about" section
 aboutEl.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
   scrollTo({top:250, left: 0, behavior: "smooth"});
 });
 
+// When the "technology" link is clicked, then the browser will scroll to the "technology" section
 techEl.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
   scrollTo({top: 840, left: 0, behavior: "smooth"});
 });
 
+// When the "project" link is clicked, then the browser will scroll to the "project" section
 projectEl.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
   scrollTo({top: 1275, left: 0, behavior: "smooth"});
 });
 
+// When the "contact" link is clicked, then the browser will scroll to the "contact" section
 contactEl.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
   scrollTo({top: 3000, left: 0, behavior: "smooth"});
 });
 
+// When the "contact" link is clicked, then the browser will scroll to the "contact" section
 socialEl.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
   scrollTo({top: 3000, left: 0, behavior: "smooth"});
