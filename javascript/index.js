68cbbd509ef62021f4217944b35cce7935da8b16
Lawrence C <lawrencecaudle870@gmail.com>
2021-12-06 19:16:27 -0600

    deleted code from html, scss, and javascript

--- javascript/index.js
+++ javascript/index.js
@@ -8,24 +8,6 @@ const contactEl = document.querySelector('.contact');
 const socialEl = document.querySelector('.social');
 const navNameEl = document.querySelector('.navigation-name');
 
-function myFunction() {
-  document.querySelector(".myDropDown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-} 
-
-
 // To close the navigation window
 closeButton.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
